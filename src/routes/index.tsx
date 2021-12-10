e457e97a203eb612a3648988271cbb5976753512
Adson Novaes <adsonnovaes7@gmail.com>
2021-12-10 15:34:11 -0300

    code refactoring

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,23 @@
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-  } from "react-router-dom";
-  import SignIn from '../pages/SignIn';
-  import SignUp from '../pages/SignUp';
-  import Dashboard from '../pages/Dashboard';
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-  export const Router = ()=>{
-    return(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />}/>
-          <Route path="/signup" element={<SignUp />}/>
-          <Route path="/dashboard" element={<Dashboard />}/>
-        </Routes>
-      </BrowserRouter>
-    )
-  }
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
-  export default Router
-  
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
